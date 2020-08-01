6f768e502a997988ece35884a5f356b9278e44fc
mermadly <victoria.echavarria@possible.com>
2020-08-01 00:07:28 -0300

    Card Item fetch

--- client/src/Item/Item.js
+++ client/src/Item/Item.js
@@ -1,46 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TruckIcon from "../img/ic_shipping.png";
 import { BrowserRouter as Link } from "react-router-dom";
 import "./Item.scss";
 
-class Item extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Item = (props) => {
+  const [product, setProduct] = useState({});
+  const [productDescription, setProductDescription] = useState({});
 
-  render() {
-    return (
-      <div className="Item">
-        {console.log(this.props.location.state.data)}
-        <div className="itemData">
-          <div className="itemImage">
-            <img src={this.props.location.state.data.thumbnail} alt="" />
-          </div>
-          <div className="itemDetails">
-            <div>
-              <span className="estadoItem">
-                {this.props.location.state.data.condition === "new"
-                  ? "Nuevo "
-                  : "Usado"}
+  const search = () => {
+    fetch(`https://api.mercadolibre.com/items/${props.match.params.id}`)
+      .then((respuesta) => {
+        respuesta
+          .json()
+          .then((info) => {
+            setProduct(info);
+            console.log(info);
+            console.log(product);
+          })
+          .catch((err) => {});
+      })
+      .catch((err) => {});
+  };
+
+  const searchDescription = () => {
+    fetch(
+      `https://api.mercadolibre.com/items/${props.match.params.id}/description`
+    )
+      .then((respuesta) => {
+        respuesta
+          .json()
+          .then((info) => {
+            setProductDescription(info);
+          })
+          .catch((err) => {});
+      })
+      .catch((err) => {});
+  };
+
+  useEffect(() => {
+    search();
+    searchDescription();
+  }, []);
+
+  return (
+    <div className="Item">
+      <div className="itemData">
+        <div className="itemImage">
+          <img src={props.location.state.data.thumbnail} alt="" />
+        </div>
+        <div className="itemDetails">
+          <div>
+            <span className="estadoItem">
+              {props.location.state.data.condition === "new"
+                ? "Nuevo"
+                : "Usado"}
+            </span>
+            {props.location.state.data.sold_quantity !== 0 ? (
+              <span className="ventasItem">
+                - {props.location.state.data.sold_quantity} vendidos
               </span>
-              {this.props.location.state.data.sold_quantity !== 0 ? (
-                <span className="ventasItem">
-                  - {this.props.location.state.data.sold_quantity} vendidos
-                </span>
-              ) : null}
-            </div>
-            <div className="itemName">
-              {this.props.location.state.data.title}
-            </div>
-            <div className="itemPrice">
-              ${this.props.location.state.data.price}
-            </div>
+            ) : null}
           </div>
+          <div className="itemName">{props.location.state.data.title}</div>
+          <div className="itemPrice">${props.location.state.data.price}</div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Item;
--- client/src/Productos/Productos.js
+++ client/src/Productos/Productos.js
@@ -14,7 +14,7 @@ const Productos = () => {
 
   useEffect(() => {
     search();
-  }, []);
+  }, [location.search]);
 
   const search = () => {
     let query = getSearchParams();
